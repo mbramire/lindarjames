c93afbcd56ac73f0edb65693e3d20102952abf0e
Matt Ramirez <mbramire@live.com>
2014-03-22 11:43:02 -0400

    first commit, gems installed, index route created

---
+++ Gemfile
@@ -0,0 +1,19 @@
+ruby '2.0.0'
+source "https://rubygems.org"
+ 
+gem "sinatra"
+gem "pg"
+gem "activerecord"
+gem "sinatra-activerecord"
+gem "sass"
+gem "haml"
+gem "carrierwave"
+gem 'bcrypt-ruby'
+gem "tux"
+gem "dotenv"
+gem "fog", "~> 1.3.1"
+gem "pony"
+
+group :development do
+  gem "shotgun"
+end
---
+++ app.rb
@@ -0,0 +1,33 @@
+require 'dotenv'
+Dotenv.load
+
+require 'sinatra'
+require 'sinatra/activerecord'
+require 'bcrypt'
+require 'carrierwave'
+require 'carrierwave/orm/activerecord'
+require 'pony'
+
+Pony.options = {
+  :via => :smtp,
+  :via_options => {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :domain => 'heroku.com',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+}
+
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/ddl3r9urh0p0sb')
+
+enable :sessions
+
+# require_relative 'config/initializers/carrierwave'
+# require_relative 'uploaders/image_uploader'
+# require_relative 'models/models'
+# require_relative 'helpers/helpers'
+require_relative 'routes/routes'
+
