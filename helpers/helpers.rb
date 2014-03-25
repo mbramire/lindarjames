eec204c3d101a4286bafbd555ef649d6a544402c
Matt Ramirez <mbramire@live.com>
2014-03-24 21:15:10 -0400

    hoempage

---
+++ helpers/helpers.rb
@@ -0,0 +1,21 @@
+helpers do
+  # If @title is assigned, add it to the page's title.
+  def title
+    if @title
+      "#{@title} - Linda R James"
+    else
+      "The Artwork of Linda R James"
+    end
+  end
+ 
+  # Format the Ruby Time object returned from a post's created_at method
+  # into a string that looks like this: 06 Jan 2012
+  def pretty_date(time)
+   time.strftime("%B %d, %Y")
+  end
+
+  #session helpers
+  def admin_present?
+    session[:user].present?
+  end
+end
